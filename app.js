e64db954d035f1456385eb88a5f39a7bd35b588b
amidabrian51 <briankaty1@me.com>
2018-04-07 23:03:13 +0100

    Adding event listeners

--- app.js
+++ app.js
@@ -8,3 +8,15 @@ const result_div = document.querySelector(".result");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
+
+rock_div.addEventListener('click', function(){
+    console.log("You click on the rock");
+});
+
+paper_div.addEventListener('click', function(){
+    console.log("You click on the paper");
+});
+
+scissors_div.addEventListener('click', function(){
+    console.log("You click on the scissors");
+});
