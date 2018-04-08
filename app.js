80bc7fc755511ea299e03d10c46276acebc9408c
amidabrian51 <briankaty1@me.com>
2018-04-08 19:28:53 +0100

    Added ES6 to glow functions

--- app.js
+++ app.js
@@ -22,29 +22,37 @@ function convertToWord(letter) {
     return "Scissors";
 }
 function win(userChoice, computerChoice) {
+    const smallUserWord = "user".fontcolor("red").fontsize(3).sub();
+    const smallCompWord = "comp".fontcolor("blue").fontsize(3).sub();
+    const userChoice_div = document.getElementById(userChoice);
     userScore++;
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = computerScore;
-    const smallUserWord = "user".fontcolor("red").fontsize(3).sub()
-    const smallCompWord = "comp".fontcolor("blue").fontsize(3).sub()
     result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} beats ${convertToWord(computerChoice)}${smallCompWord} You win!`;
-    
+    userChoice_div.classList.add('green-glow');
+    setTimeout(() => userChoice_div.classList.remove('green-glow'), 300);
 }
 
 function lose(userChoice, computerChoice) {
+    const smallUserWord = "user".fontcolor("red").fontsize(3).sub()
+    const smallCompWord = "comp".fontcolor("blue").fontsize(3).sub()
+    const userChoice_div = document.getElementById(userChoice);
     computerScore++;
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = computerScore;
-    const smallUserWord = "user".fontcolor("red").fontsize(3).sub()
-    const smallCompWord = "comp".fontcolor("blue").fontsize(3).sub()
     result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} loses ${convertToWord(computerChoice)}${smallCompWord} You lost!`;
+    userChoice_div.classList.add('red-glow');
+    setTimeout(() =>  userChoice_div.classList.remove('red-glow'), 300);
 }
 
 
 function draw(userChoice, computerChoice) {
-    const smallUserWord = "user".fontcolor("red").fontsize(3).sub()
-    const smallCompWord = "comp".fontcolor("blue").fontsize(3).sub()
+    const smallUserWord = "user".fontcolor("red").fontsize(3).sub();
+    const smallCompWord = "comp".fontcolor("blue").fontsize(3).sub();
+    const userChoice_div = document.getElementById(userChoice);
     result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} equal ${convertToWord(computerChoice)}${smallCompWord} Its a draw`;
+    userChoice_div.classList.add('gray-glow');
+    setTimeout(() => userChoice_div.classList.remove('gray-glow'), 300);
 }
 
 
