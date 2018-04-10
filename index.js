1e87fecaa50493943d27e4450795feef07f72a77
amidabrian51 <briankaty1@me.com>
2018-04-10 16:49:56 +0100

    make it better

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ function onRequest(request, response) {
         response.end('');
     } 
 }
-http.createServer(onRequest).listen(8888);
+http.createServer(onRequest).listen(3000);
 console.log("Server has started.");
