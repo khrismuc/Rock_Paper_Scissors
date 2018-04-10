8c88cc9c807c8c91d1051ac5532eb7fb1d574761
amidabrian51 <briankaty1@me.com>
2018-04-10 16:15:40 +0100

    Added structure as well got it to work on a nodejs server

---
+++ index.js
@@ -0,0 +1,14 @@
+var http = require("http");
+var route = require('./route');
+
+var html;
+
+function onRequest(request, response) {
+  if (request.method === 'GET') {
+        response.writeHead(200);
+        response.write(route.getFile(request.url));
+        response.end('');
+    } 
+}
+http.createServer(onRequest).listen(8888);
+console.log("Server has started.");
